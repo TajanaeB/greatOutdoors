51e322eb116e4ad5e16ac58b82d5d9a0489d5c41
Tajanae Boone <Tajanaeboone@yahoo.com>
2021-09-13 14:55:31 -0400

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { AddProductComponent } from './add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
