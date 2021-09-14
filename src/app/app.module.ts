97ab044f6d3463b174664d1d2387a2b64026e24d
Tajanae Boone <Tajanaeboone@yahoo.com>
2021-09-14 13:58:41 -0400

    added header, footer, login, main component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,30 @@ import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
 import { AddProductComponent } from './add-product/add-product.component';
 import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    AddProductComponent
+    AddProductComponent,
+    HeaderComponent,
+    SideMenuComponent,
+    MainComponent,
+    FooterComponent,
+    LogInComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
